a2741fc254f3914e12aed403bc5f7ed5799d3163
Stanly Johnson <stanlyjohnson@outlook.com>
2020-06-26 22:45:47 +0530

    basic market contract

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -5,11 +5,18 @@ contract Market {
     struct Item {
         uint uid;
         string name;
+        string link;
         uint256 price;
     }
 
     // create a var to store the store owner address
-    address constant public store_owner = address(0x5660df1681a32E70704439E9243b1B91c369580e);
+    address payable store_owner = address(0x5660df1681a32E70704439E9243b1B91c369580e);
+
+    // event to signal purchase
+    event Purchase(uint256 _uid);
+
+    // event to signal item creation
+    event ItemCreated(uint256 _uid);
 
     // Create map to items list
     mapping(uint => Item) public items;
@@ -19,17 +26,37 @@ contract Market {
 
     constructor () public {
         // create a test candidate
-        createItem("BookX", 10000000000000000);
+        createItem("BookX", 10000000000000000, "stahshsidid");
+    }
+
+    modifier onlyOwner() {
+        // ensure the store owner is the msg.sender
+        require(msg.sender == store_owner, "Only owner can create asset");
+        _;
     }
 
     //funtion to register new candidate
-    function createItem (string memory _name, uint256 _price) public {
-        // ensure the store owner is the msg.sender
-        require(msg.sender == store_owner);
+    function createItem (string memory _name, uint256 _price, string memory _link) public onlyOwner {
         // create uid for item
         itemCount++;
         // create the item
-        items[itemCount] = Item(itemCount, _name, _price);
+        items[itemCount] = Item(itemCount, _name, _link, _price);
+        //trigger item created event
+        emit ItemCreated(itemCount);
+    }
+
+    function purchaseItem (uint256 _uid) public payable returns (string memory link){
+        // check if the value sent by caller is equal to price
+        require(msg.value == items[_uid].price, "Price Mismatch");
+        // trigger item purchase event
+        emit Purchase(_uid);
+        // return the link to download the item
+        return (items[_uid].link);
+    }
+
+    function withdrawBalance() public onlyOwner {
+        // transfer balance
+        store_owner.transfer(address(this).balance);
     }
 
 
