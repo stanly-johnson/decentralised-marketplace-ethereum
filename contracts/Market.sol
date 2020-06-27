cd27477a1fce26ef6649a701be1af593b058c242
Stanly Johnson <stanlyjohnson@outlook.com>
2020-06-27 08:47:55 +0530

    updated contract

--- contracts/Market.sol
+++ contracts/Market.sol
@@ -5,21 +5,27 @@ contract Market {
     struct Item {
         uint uid;
         string name;
-        string link;
         uint256 price;
+    }
+
+    struct ItemDetail {
+        uint uid;
+        string link;
     }
 
     // create a var to store the store owner address
     address payable public store_owner = address(0x5660df1681a32E70704439E9243b1B91c369580e);
 
     // event to signal purchase
-    event Purchase(uint256 _uid);
+    event Purchase(uint256 _uid, string _link);
 
     // event to signal item creation
     event ItemCreated(uint256 _uid);
 
     // Create map to items list
     mapping(uint => Item) public items;
+    // Create map to items list
+    mapping(uint => ItemDetail) private itemDetails;
 
     // Store Items Count
     uint public itemCount;
@@ -40,18 +46,17 @@ contract Market {
         // create uid for item
         itemCount++;
         // create the item
-        items[itemCount] = Item(itemCount, _name, _link, _price);
+        items[itemCount] = Item(itemCount, _name, _price);
+        itemDetails[itemCount] = ItemDetail(itemCount, _link);
         //trigger item created event
         emit ItemCreated(itemCount);
     }
 
-    function purchaseItem (uint256 _uid) public payable returns (string memory link){
+    function purchaseItem (uint256 _uid) public payable {
         // check if the value sent by caller is equal to price
         require(msg.value == items[_uid].price, "Price Mismatch");
         // trigger item purchase event
-        emit Purchase(_uid);
-        // return the link to download the item
-        return (items[_uid].link);
+        emit Purchase(_uid, itemDetails[_uid].link);
     }
 
     function withdrawBalance() public onlyOwner {
