efbcec3b81ca74296c33987da314cc2817fdb4f4
Stanly Johnson <stanlyjohnson@outlook.com>
2020-07-12 00:20:20 +0530

    navigation changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Switch, Redirect, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home'
+import ListItem from './Components/ListItem'
 
 function App() {
   return (
     <BrowserRouter>
       <main className="container">
           <Switch>
+            <Route path="/list" component={ListItem} />
             <Route path="/" component={Home} />
             <Redirect to="/404" />
           </Switch>
